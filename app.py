748e963263c50049e08d03ac0ab1dc5fbfd0df91
curtisgraber-bps <curtisgraber@gmail.com>
2026-04-12 20:24:41 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,22 @@ import pandas as pd
 
 st.title("Vacation Scheduler")
 
-st.header("Employee List")
-
 df = pd.read_csv("employees.csv")
+
+st.header("Select Your Vacation Weeks")
+
+# Dropdown of employees
+employee = st.selectbox(
+    "Select Your Name",
+    df["Employee First Name"] + " " + df["Employee Last Name"]
+)
+
+choice1 = st.text_input("First Choice")
+choice2 = st.text_input("Second Choice")
+choice3 = st.text_input("Third Choice")
+
+if st.button("Submit"):
+    st.success(f"Submitted for {employee}")
+
+st.header("Employee List")
 st.write(df)
